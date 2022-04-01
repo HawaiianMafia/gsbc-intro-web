85f421b2ef7de2f8f0bdedaf3b6e253d80584ab2
HawaiianMafia <98447212+HawaiianMafia@users.noreply.github.com>
2022-04-01 08:34:58 -0700

    array integers

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -1,1 +1,6 @@
 
+function getRandomInt(min, max) {
+  min = Math.ceil(1);
+  max = Math.floor(100);
+  return Math.floor(Math.random() * (100 - 1) + 1); //The maximum is exclusive and the minimum is inclusive
+}
