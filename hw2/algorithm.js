dac3d4cf4cb92e44e5fecdc94e32b29b7eaaf690
HawaiianMafia <98447212+HawaiianMafia@users.noreply.github.com>
2022-04-02 18:22:06 -0700

    Array testing

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -1,1 +1,16 @@
 
+let raNums = []
+let total = 0 
+let p1 = document.createElement("p")
+document.body.appendChild(p1)
+for (let x = 1; x <= 100; x++) {
+    let rndNum = parseInt(Math.random() * (1000 -1) + 1)
+    let p = document.createElement("p")
+    p.textContent = rndNum
+    raNums.push(rndNum)
+    total += rndNum
+    //document.getElementbyId("numbers").appendChild(p)
+    document.body.appendChild(p)
+}
+document.getElementById("maxi").textContent = "Max Number: " + Math.max(...raNums)
+document.getElementById("sumof".textContent = "Grand Total: " + total
