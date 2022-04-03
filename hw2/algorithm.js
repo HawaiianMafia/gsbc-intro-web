0cbabafb6c9080b4a50e4406177233501d5026c7
HawaiianMafia <98447212+HawaiianMafia@users.noreply.github.com>
2022-04-02 21:55:34 -0700

    missing info added

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -1,7 +1,7 @@
-
 let raNums = []
 let total = 0 
 let p1 = document.createElement("p")
+pl.textContent = ".";
 document.body.appendChild(p1)
 for (let x = 1; x <= 100; x++) {
     let rndNum = parseInt(Math.random() * (1000 -1) + 1)
