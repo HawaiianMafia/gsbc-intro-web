6af61fccc819dedecc86a55065e9ce34322021bf
HawaiianMafia <98447212+HawaiianMafia@users.noreply.github.com>
2022-04-02 22:29:41 -0700

    Update algorithm.js

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -1,16 +1,16 @@
 let raNums = []
-let total = 0 
+let total = 0
 let p1 = document.createElement("p")
-pl.textContent = ".";
+p1.textContent = ".";
 document.body.appendChild(p1)
 for (let x = 1; x <= 100; x++) {
-    let rndNum = parseInt(Math.random() * (1000 -1) + 1)
+    let rndNum = parseInt(Math.random() * (1000 - 1) + 1)
     let p = document.createElement("p")
     p.textContent = rndNum
     raNums.push(rndNum)
     total += rndNum
-    //document.getElementbyId("numbers").appendChild(p)
+    //document.getElementById("numbers").appendChild(p)
     document.body.appendChild(p)
 }
-document.getElementById("maxi").textContent = "Max Number: " + Math.max(...raNums)
-document.getElementById("sumof").textContent = "Grand Total: " + total
+document.getElementById("largest").textContent = "Maximum Number: " + Math.max(...raNums)
+document.getElementById("sum").textContent = "Total: " + total
