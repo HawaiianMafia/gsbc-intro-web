33a19672e5f97ef1af76b6e1f86dd07efe4401aa
HawaiianMafia <98447212+HawaiianMafia@users.noreply.github.com>
2022-04-22 01:49:57 -0700

    Update app.js

--- Hw3/app.js
+++ Hw3/app.js
@@ -1,7 +1,17 @@
 let div = document.getElementById('content');
 let canvas = document.createElement("canvas");
 div.appendChild(canvas);
-<script>
+  <img id="image" src=
+"i-am-lost-5cd1a6.jpg"
+            alt="Lost Image" />
+    </div>
+ 
+    <button type="button"
+        onclick="show()" id="btnID">
+        Show Image
+    </button>
+ 
+    <script>
         function show() {
  
             
