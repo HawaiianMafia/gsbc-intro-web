a3570c410400cea2e82f9ca9d9955225594aa8e9
HawaiianMafia <98447212+HawaiianMafia@users.noreply.github.com>
2022-04-22 01:46:20 -0700

    Update app.js

--- Hw3/app.js
+++ Hw3/app.js
@@ -1,3 +1,14 @@
 let div = document.getElementById('content');
 let canvas = document.createElement("canvas");
 div.appendChild(canvas);
+<script>
+        function show() {
+ 
+            
+            document.getElementById('image')
+                    .style.display = "block";
+ 
+            document.getElementById('btnID')
+                    .style.display = "none";
+        }
+    </script>
